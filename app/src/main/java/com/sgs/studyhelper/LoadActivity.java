34d7dcb0b11186fd5af9eb1dadfe3a06dfe941e4
BoredBear <xfvladikxf@gmail.com>
2018-02-18 17:21:10 +0300

    Added pictures

--- app/src/main/java/com/sgs/studyhelper/LoadActivity.java
+++ app/src/main/java/com/sgs/studyhelper/LoadActivity.java
@@ -1,5 +1,6 @@
 package com.sgs.studyhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class LoadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+
+        // Start loading
+
+        // End loading
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
