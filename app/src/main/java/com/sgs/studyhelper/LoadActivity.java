0491f7f805d22d32ba3c15d2cced47b99ffa023f
BoredBear <xfvladikxf@gmail.com>
2018-03-02 15:58:20 +0300

    Changes in load Layout

--- app/src/main/java/com/sgs/studyhelper/LoadActivity.java
+++ app/src/main/java/com/sgs/studyhelper/LoadActivity.java
@@ -6,9 +6,18 @@ import android.content.SharedPreferences;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import java.util.zip.Inflater;
+
 public class LoadActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    String pickedLanguage = "Russian";
+    boolean isLangPicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +25,11 @@ public class LoadActivity extends AppCompatActivity {
 
         boolean isFirstStart = false;
 
-        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
         if(sharedPreferences.getBoolean("isFirstStart", true)) {
             setContentView(R.layout.activity_firststart);
-            createButtonListeners();
+            createLangPickButtonListeners();
+            createNextButtonListener();
             isFirstStart = true;
         }
         else
@@ -56,8 +66,44 @@ public class LoadActivity extends AppCompatActivity {
         }
     }
 
-    protected void createButtonListeners()
+    protected void createNextButtonListener() {
+        Button nextBt = findViewById(R.id.bt_next);
+        nextBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isLangPicked)
+                {
+                    // TODO Save design and load menu
+                } else
+                {
+                    isLangPicked = true;
+                    sharedPreferences.edit().
+                            putString("Language", pickedLanguage).
+                            apply();
+                    // TODO Load next include activity (Design)
+                }
+            }
+        });
+    }
+
+    protected void createLangPickButtonListeners()
     {
+        RadioGroup rg = findViewById(R.id.rg_lang);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.rb_russian)
+                {
+                    pickedLanguage = "Russian";
+                } else if(checkedId == R.id.rb_english)
+                {
+                    pickedLanguage = "English";
+                }
+            }
+        });
+    }
+
+    protected void createButtonListeners() {
         RadioGroup radioGroup = findViewById(R.id.rg_design);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -65,7 +111,7 @@ public class LoadActivity extends AppCompatActivity {
                 switch (checkedId)
                 {
                     case 0:
-
+                        // TODO Setup current design
                         break;
                     case 1:
 
@@ -79,11 +125,6 @@ public class LoadActivity extends AppCompatActivity {
                 }
             }
         });
-
-    }
-
-    protected void bt_GoToMenuPressed()
-    {
 
     }
 }
